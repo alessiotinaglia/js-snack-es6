8ac67b64afef0508a20dc3f63614e5df0bf2e8a5
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-12 15:53:31 +0100

    sistemazione cartelle e inizio es 2

---
+++ esercizio 2/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+//  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+
+// <!-- Snack 2 -->
+
+const biciclette = [
+    {
+        nome: "Milan",
+        puntiFatti: 15,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Atalanta",
+        puntiFatti: 15,
+        falliSubiti
+    },
+    {
+        nome: "cagliari",
+        puntiFatti: 15,
+        falliSubiti
+    },
+    {
+        nome: "torino",
+        puntiFatti: 15,
+        falliSubiti
+    },
+];
+
+
+
