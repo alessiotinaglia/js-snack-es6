9320991ef66f8f4320fa51b2b6f595207919e5c5
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-12 15:48:29 +0100

    fine es 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 //  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
 //  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
@@ -8,7 +8,7 @@
 const biciclette = [
     {
         nome: "Bianchi",
-        peso: 35,
+        peso: 15,
     },
     {
         nome: "Atala",
@@ -23,4 +23,25 @@ const biciclette = [
         peso: 45,
     },
 ];
+
+// 
+let biciLeggera = biciclette[0];
+for (let i = 0; i < biciclette.length; i++) {
+    if (biciclette[i].peso < biciLeggera.peso) {
+        biciLeggera = biciclette[i];
+    }
+}
+console.log(biciLeggera);
+
+const main = document.querySelector("main")
+
+const template = `
+<div className="card">
+<h4 class="card-title">La bici che pesa di meno è : ${biciLeggera.nome}</h4>
+<div>Il peso della bici è : ${biciLeggera.peso}</div>
+
+</div>
+`;
+
+main.innerHTML += template;
 
